42999b468221ce2860688bc8a468f8ac51bb6769
jdg824 <80143957+jdg824@users.noreply.github.com>
2022-11-14 01:11:44 +0900

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+d = []
+
+for i in range(10):
+    d.append([])
+    for j in range(10):
+        d[i].append(0)
+
+for i in range(10):
+    n = input().split()
+    for j in range(10):
+        d[i][j] = int(n[j])
+
+r=1
+c=1
+
+while True:
+    if d[r][c+1]==0 or d[r][c+1]==2:
+        d[r][c]=9
+        c+=1
+    elif d[r+1][c] ==0 or d[r+1][c]==2:
+        d[r][c]=9
+        r+=1
+    
+    if (d[r][c]==2):
+        d[r][c]=9
+        break
+    elif (d[r+1][c]==1 and d[r][c+1]==1) or (r==9 and c==9):
+        break
+    
+for i in range(10):
+    for j in range(10):
+        print(d[i][j], end=' ')
+    print()
